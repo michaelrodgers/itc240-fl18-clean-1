e4ee422bdb67e7fb5b8f54bec7fa350617eacd3f
Michael Rodgers <38140530+michaelrodgers@users.noreply.github.com>
2018-11-13 15:49:16 -0800

    Delete credentials.php

--- clean/credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','itc240');
-define('DB_USER','michaelrodgers@itc240');
-define('DB_PASSWORD','ChicaGo6290**');
-define('DB_HOST','itc240.mysql.database.azure.com');
-
-?>
